b9edf733b9ce6f2eb1fa1aa27f7323b66cba07c1
Mario Fabre <66124986+mariotafner@users.noreply.github.com>
2021-05-06 21:58:36 -0300

    Release 1

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ import json
 email = ""
 password = ""
 
+print("iniciando")
+
+valor = random.randint(0, 100000)
+
+response = requests.get("https://www.thiswaifudoesnotexist.net/example-" + str(valor) + ".jpg")
+if (response.status_code == 200):
+    file = open("profile.png", "wb")
+    file.write(response.content)
+    file.close()
+
 with open('login.json') as json_file:
     data = json.load(json_file)
     email = data['email']
@@ -26,7 +36,7 @@ browser = webdriver.Firefox(options=options)
 
 browser.get('https://discord.com/app')
 
-time.sleep(5)
+time.sleep(3)
 
 inputEmail = browser.find_element_by_name("email")
 inputEmail.send_keys(email)
@@ -38,20 +48,23 @@ inputPassword.send_keys(password)
 time.sleep(1)
 inputPassword.send_keys(Keys.ENTER)
 
-valor = random.randint(0, 100000)
+time.sleep(5)
+browser.find_element(By.XPATH, "//*[@aria-label='Configurações de Usuário']").click()
 
-response = requests.get("https://www.thiswaifudoesnotexist.net/example-" + str(valor) + ".jpg")
+time.sleep(3)
+browser.find_element(By.XPATH, 
+    "/html/body/div/div[2]/div/div[2]/div[2]/div/div[2]/div/div/main/div/div[1]/div/div/div[1]/div[1]/div/input"
+    ).send_keys("C:\\dev\\random-discord-profile\\profile.png")
 
-if (response.status_code == 200):
-    #try:
-    #    os.remove("profile.png")
-    #except IOError:
-    #    print("Não encontrada")
+time.sleep(3)
+browser.find_element(By.XPATH, "//*[text()='Aplicar']").click()
 
-    file = open("profile.png", "wb")
-    file.write(response.content)
-    file.close()
+time.sleep(3)
+browser.find_element(By.XPATH, "//*[text()='Salvar alterações']").click()
+time.sleep(2)
 
-a = input()
+browser.close()
+
+
 
 
